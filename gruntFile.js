42b2d52b13ee22ca67fc9b0c4f827b537576d632
Krist Wongsuphasawat <krist.wongz@gmail.com>
2015-07-03 23:30:03 -0700

    update grunt file

--- gruntFile.js
+++ gruntFile.js
@@ -80,7 +80,7 @@ module.exports = function (grunt) {
         tagName: 'v%VERSION%',
         tagMessage: 'Version %VERSION%',
         push: true,
-        pushTo: '',
+        pushTo: 'origin',
         gitDescribeOptions: '--tags --always --abbrev=1 --dirty=-d',
         globalReplace: false,
         prereleaseName: false,
